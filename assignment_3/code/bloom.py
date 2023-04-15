9d9b92dee6eded80b48103ab8d09083141858512
Øyvind <oyvinhha@stud.ntnu.no>
2023-04-15 13:53:04 +0200

    task 3.1

--- assignment_3/code/bloom.py
+++ assignment_3/code/bloom.py
@@ -3,6 +3,8 @@
 import configparser  # for reading the parameters file
 from pathlib import Path  # for paths of files
 import time  # for timing
+import math
+import random
 
 # Global parameters
 parameter_file = 'default_parameters.ini'  # the main parameters file
@@ -38,7 +40,7 @@ def bloom_filter(new_pass):
 def read_data(file):
     time_sum = 0
     pass_read = 0
-    with file.open() as f:
+    with file.open(encoding="UTF-8") as f:
         for line in f:
             pass_read += 1
             new_password = line[:-3]
@@ -49,22 +51,48 @@ def read_data(file):
 
     return pass_read, time_sum
 
+def get_h_prime_numbers(start,end,h):
+    primes=[]
+    for i in range(start,end):
+        prime=True
+        for j in range(2,math.ceil(i/2)+1):
+            if i%j==0:
+                prime=False
+        if prime:
+            primes.append(i)
+        prime
+
+    return random.sample(primes, h)
+
 
 # TASK 1
 # Created h number of hash functions
-def hash_functions():
+def hash_function(s:str,p:int,n:int):
+    
+    s = [ord(c) for c in s]
+    the_sum=0
+    for count,ascii_of_letter in enumerate(s):
+        the_sum+=ascii_of_letter*p**count
+    answer=the_sum%n
+    return answer
 
-    # implement your code here
 
-    return 0
-
+def hash_functions(s:str,primes:list[int],n:int):
+    return_list=[]
+    for prime in primes:
+        return_list.append(hash_function(s,prime,n))
+    return return_list
+        
 
 if __name__ == '__main__':
     # Reading the parameters
     read_parameters()
-
+    print("parameters_dictionary",parameters_dictionary)
     # Creating the hash functions
-    hash_functions()
+    h_primes=get_h_prime_numbers(2,100,parameters_dictionary['h'])
+
+    hashet=hash_functions("asdf",h_primes,parameters_dictionary['n'])
+    print(hashet)
 
     # Reading the data
     print("Stream reading...")
